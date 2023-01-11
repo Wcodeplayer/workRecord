fbc3e27819c14ccd2df32ee8d8f28390e4019321
Wcodeplayer <108792471+Wcodeplayer@users.noreply.github.com>
2023-01-11 11:20:35 +0800

    添加备注，同步更新workList数据主键

--- src/store/workModule.js
+++ src/store/workModule.js
@@ -1,5 +1,5 @@
 export default {
-    namespaced:true,
+    namespaced: true,
     state: {
         titleList: [ //workList 组件的标题栏参数
             {
@@ -15,67 +15,67 @@ export default {
                 itemClass: "item"
             }
         ],
-        workList: [ // workList 组件的任务列表
-            {
-                workId: 0,
-                statu: 0,//0 : start , 1 : stop , 2 : delay , 3:end
-                list: [
-                    {
-                        title: "任务",
-                        value: "workName",
-                        itemClass: "shortItem"
-                    },
-                    {
-                        title: "开始时间",
-                        value: "2023年1月1日",
-                        itemClass: "item"
-                    },
-                    {
-                        title: "结束时间",
-                        value: "2023年1月3日",
-                        itemClass: "item"
-                    }
-                ]
-            }
+        workList: [ 
+            // workList 组件的任务列表
+            //  --  each statu  --
+            // date : dateMessage,
+            // statu: 0,//0 : start , 1 : stop , 2 : delay , 3:end
+            // list: [
+            //     {
+            //         title: "任务",
+            //         value: this.form.name,
+            //         itemClass: "shortItem"
+            //     },
+            //     {
+            //         title: "开始时间",
+            //         value: startTime,
+            //         itemClass: "item"
+            //     },
+            //     {
+            //         title: "结束时间",
+            //         value: "",
+            //         itemClass: "item"
+            //     }
+            // ]
         ],
     },
     mutations: {
         // workList_methods
-        work_finish(state,id) {
+        work_finish(state, id) {
             state.workList.forEach(element => {
                 if (element.workId == id) {
                     element.statu = 2;
                 }
             });
         },
-        work_delete(state,id) {
+        work_delete(state, id) {
             state.workList = state.workList.fillter(element => {
                 return element.workId != id
             });
         },
-        work_delay(state,id) {
+        work_delay(state, id) {
             state.workList.forEach(element => {
                 if (element.workId == id) {
                     element.statu = 1;
                 }
             });
         },
-        work_add(state,workMessage){
+        work_add(state, workMessage) {
             state.workList.push(workMessage);
         }
     },
     actions: {
-        workFinish({commit},id){
-            commit('work_finish',id);
+        workFinish({ commit }, id) {
+            commit('work_finish', id);
         },
-        workDelay({commit},id) {
-            commit('work_delay',id);
+        workDelay({ commit }, id) {
+            commit('work_delay', id);
         },
-        workDelete({commit},id ){
-            commit('work_delete',id);
+        workDelete({ commit }, id) {
+            commit('work_delete', id);
         },
-        workAdd({commit} , workMessage){
-            commit('work_add',workMessage);
+        workAdd({ commit }, workMessage) {
+            commit('work_add', workMessage);
         }
     }
 }
